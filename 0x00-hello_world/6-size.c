46ba5de6fe527d64af8e9dd831bbd323c5d1b473
Your Name <youemail.com>
2023-06-19 07:51:26 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ long int;
 long long int;
 float;
 printf("size of char: %zu byte(s)\n", sizeof(char));
-printf("size of int: %zu byte(s)\n", sizeof(int));
+printf("size of an int: %zu byte(s)\n", sizeof(int));
 printf("size of long int: %zu byte(s)\n", sizeof(long int));
 printf("size of long long int: %zu byte(s)\n", sizeof(long long int));
 printf("size of float: %zu byte(s)\n", sizeof(float));
