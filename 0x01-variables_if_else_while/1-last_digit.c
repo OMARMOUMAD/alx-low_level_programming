50a4b0697f8413d4350bd20d85da3bc9b15e1284
Your Name <youemail.com>
2023-06-19 16:32:11 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,21 +9,23 @@
 int main(void)
 {
 int n;
+int m;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("%d", n);
-if (n > 0)
+m = abs(n) % 10;
+printf("Last digit of %d", n);
+printf(" is %d", m);
+if (m > 5)
 {
-printf(" is positive\n");
+printf(" and is greater than 5\n");
 }
-else if (n < 0)
+else if (m == 0)
 {
-printf(" is negative\n");
+printf(" and is 0\n");
 }
 else
 {
-printf(" is zero\n");
+printf(" and is less than 6 and not 0\n");
 }
 return (0);
 }
-
