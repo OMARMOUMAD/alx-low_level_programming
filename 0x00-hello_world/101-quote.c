25f0057d0138a00d98a05dd2158d39b8332d99dc
Your Name <youemail.com>
2023-06-19 18:52:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 , stdout);
-return (0);
+return (1);
 }
