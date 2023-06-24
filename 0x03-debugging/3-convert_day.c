0516cbd5749c4dec778d642a667683d68e2a4cfc
Your Name <youemail.com>
2023-06-23 19:56:23 -0700

    time

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -42,11 +42,11 @@ int convert_day(int month, int day)
         case 11:
             day = 304 + day;
             break;
-        case 12:
-            day = 334 + day;
-            break;
-        default:
-            break;
-    }
-    return (day);
+case 12:
+day = 334 + day;
+break;
+default:
+break;
+}
+return (day);
 }
